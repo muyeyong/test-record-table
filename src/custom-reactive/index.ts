3c5623559e5a14eb418e1b282cbd602234b00e8e
muyeyong <xuyongshuaige@gmail.com>
2023-03-23 23:43:12 +0800

    feat: 1

--- src/custom-reactive/index.ts
+++ src/custom-reactive/index.ts
@@ -1,4 +1,4 @@
-import { customRef, getCurrentInstance, getCurrentScope } from 'vue'
+import { customRef, getCurrentInstance, getCurrentScope, watch } from 'vue'
 
 // 作用域的记录
 /**
@@ -10,40 +10,65 @@ import { customRef, getCurrentInstance, getCurrentScope } from 'vue'
  *  可以返回变化记录, 返回多个，嵌套？ 返回同级 以及子级的log
  *  联合变量？ 表格？
  */
-// value应该是 key value
-const history = new Map()
-const activeComponent = null
-
-const record = (target: any, value: any) => {
-    //TODO 还是要以当前组件作为target
-    // 有一个弹框弹出来，它记录的信息需要上报，关闭后，还需要上报信息
-    let dep = history.get(target)
-    if (!dep) {
-        dep = new Set()
-        history.set(target, dep)
-    }
-    if (dep.has(value)) {
-       const newDep = new Set()
-       for(let v of  dep.values()) {
-         newDep.add(v)
-         if(value === v) break
-       }
-       history.set(target, newDep)
-    } else {
-        dep.add(value)
-    }
-}
-
-export const logAll = () => {
-    console.log(history)
-}
 
 export function useReactiveRecord<T> (value: T, describe?: string) {
+    const instance = getCurrentInstance()
+    if (!instance) return
+    let history: any = null
+    if (instance.props.history) {
+        history = instance.props.history
+    } else {
+        instance.props.history = (history = new Map())
+    }
+    let reportHistoryChange: any = null
     const log = () => {
         return history
     }
+    const childHistoryChange = (childHistory: Map<any, any>) => {
+        // 监听子组件变化
+        console.log('childHistoryChange:', history, childHistory)
+        for(const [key, value] of childHistory.entries()) {
+            if (history.has(key)) {
+                // 更新
+            } else {
+                // 添加
+                history.set(key, value)
+            }
+        }
+        console.log(history)
+        // history = new Map([...history, ...childHistory])
+    }
+    const record = (target: any, value: any) => {
+        // debugger
+        //TODO 还是要以当前组件作为target
+        // 有一个弹框弹出来，它记录的信息需要上报，关闭后，还需要上报信息
+        let dep = history.get(target)
+        if (!dep) {
+            dep = new Set()
+            history.set(target, dep)
+        }
+        if (dep.has(value)) {
+           const newDep = new Set()
+           for(let v of  dep.values()) {
+             newDep.add(v)
+             if(value === v) break
+           }
+           history.set(target, newDep)
+        } else {
+            dep.add(value)
+        }
+        reportHistoryChange && reportHistoryChange(history)
+    }
+   
+    const { parent } = instance
+    // console.log(instance)
+    // 向上处理
+    reportHistoryChange = parent?.props.reportHistoryChange
+    instance.props = {...instance.props, reportHistoryChange: childHistoryChange}
+ 
+    // console.log('instance', instance.props)
+    // 需要将子组件的history引入
    const target = customRef((track, trigger) => {
-    console.log('233', getCurrentInstance())
     return {
         get() {
             track()
